cf12e5240442f2a943af48e7d611640c54ba346c
Ludvig <ludvigvonreis@gmail.com>
2022-05-05 21:34:43 +0200

    Removed old weapon system

--- Assets/_Scripts/Weapon/Stats/ShotgunStats.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-//[CreateAssetMenu(fileName = "New Shotgun", menuName = "Weapons/Shotgun")]
-public class ShotgunStats : WeaponStats
-{
-	public int totalPellets;
-	public float maxDevitation;
-}
