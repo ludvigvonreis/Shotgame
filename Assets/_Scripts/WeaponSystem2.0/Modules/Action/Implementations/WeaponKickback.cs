bfec4da030f497fd8444ab3e94a244f727e42fcc
Ludvig <ludvigvonreis@gmail.com>
2022-09-29 14:07:59 +0200

    Fixxded weapon kikbak

--- Assets/_Scripts/WeaponSystem2.0/Modules/Action/Implementations/WeaponKickback.cs
+++ Assets/_Scripts/WeaponSystem2.0/Modules/Action/Implementations/WeaponKickback.cs
@@ -7,8 +7,9 @@ namespace WeaponSystem.Actions
 	public class WeaponKickback : WeaponAction
 	{
 		[SerializeField] private Transform kickbackHolder;
-		[SerializeField] public float kickbackForce;
-		[SerializeField] public float resetSmoothing;
+		[SerializeField] private float kickbackForce;
+
+		[SerializeField] private float resetSmoothing;
 
 		private bool isShooting;
 		private bool hasReset;
@@ -18,6 +19,7 @@ namespace WeaponSystem.Actions
 			base.Init();
 
 			groupReference.Action.OnPerfom += Action;
+			groupReference.OnGroupProcess += ResetKickback;
 		}
 
 		protected override void ProcessInput(object sender, WeaponEvent.ActionContext context)
@@ -35,29 +37,18 @@ namespace WeaponSystem.Actions
 			}
 		}
 
-		void OnProcess()
+		void ResetKickback()
 		{
-			if (isShooting) return;
-			if (hasReset) return;
-
-			if (Vector3.Distance(kickbackHolder.localPosition, Vector3.zero) >= 0.1f)
-			{
-				kickbackHolder.localPosition = Vector3.Lerp(
+			kickbackHolder.localPosition = Vector3.Lerp(
 					kickbackHolder.localPosition, Vector3.zero, Time.deltaTime * resetSmoothing
 				);
-			}
-			else
-			{
-				hasReset = true;
-			}
 		}
 
 		void Action()
 		{
 			if (isShooting)
 			{
-				hasReset = false;
-				kickbackHolder.localPosition -= new Vector3(0, 0, kickbackForce);
+				kickbackHolder.localPosition -= new Vector3(0, 0, kickbackForce * Random.value);
 			}
 		}
 	}
